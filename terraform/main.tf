cb84f4f49b7ce6bb9e27f5fc6b28914e7262b545
Felipe Ramirez <framirez@bwl.com.mx>
2023-07-29 22:41:17 -0600

    feat: :rocket: Add terraform EC2 + RDS

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,34 +6,34 @@ provider "aws" {
     secret_key = "${var.secret_key}"
 }
 
-#create a security group for RDS Database Instance
-resource "aws_security_group" "rds_sg" {
-  name = "rds_sg"
-  ingress {
-    from_port       = 3306
-    to_port         = 3306
-    protocol        = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+resource "aws_vpc" "projectvpc" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "projectvpc"
   }
 }
 
-#create a RDS Database Instance
-resource "aws_db_instance" "myinstance" {
+
+resource "aws_instance" "backend" {
+  ami           = "ami-0ac80df6eff0e70b5"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "Backend"
+  }
+}
+
+resource "aws_db_instance" "projectdb" {
   engine               = "mysql"
   identifier           = "myrdsinstance"
   allocated_storage    =  20
-  engine_version       = "5.7"
+  engine_version       = "5.7.42"
   instance_class       = "db.t2.micro"
   username             = "myrdsuser"
   password             = "myrdspassword"
   parameter_group_name = "default.mysql5.7"
-  vpc_security_group_ids = ["${aws_security_group.rds_sg.id}"]
   skip_final_snapshot  = true
   publicly_accessible =  true
+  allow_major_version_upgrade = true
 }
--- terraform/terraform.tfvars
+++ terraform/terraform.tfvars
@@ -1,4 +1,4 @@
 #terraform.tfvars
 region = "us-east-1"
-access_key = "<YOUR AWS CONSOLE ACCESS ID>"
-secret_key = "<YOUR AWS CONSOLE SECRET KEY>"
+access_key = "wee"
+secret_key = "wewe/J1+"
